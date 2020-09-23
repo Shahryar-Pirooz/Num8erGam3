ff0ca0b2d1ae9723c6dd30ed6df00fc9a6da47b1
Shahryar-Pirooz <dev.shahryar@gmail.com>
2020-09-23 23:39:40 +0330

    fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:numbergame/pages/HomeApp.dart';
 import 'package:numbergame/src/colors.dart';
-import 'package:numbergame/test.dart';
 
 main() {
   runApp(MaterialApp(
     color: MyColors.blue,
-    home: Test(),
+    home: HomeApp(),
     theme: ThemeData(
         backgroundColor: MyColors.black,
         scaffoldBackgroundColor: MyColors.black),
